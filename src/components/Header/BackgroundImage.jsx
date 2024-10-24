b73aebd525ad31aac31a320b132b1b2bdd34d354
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-10-24 21:10:35 +0400

    style: add BackgroundImage in header

---
+++ src/components/Header/BackgroundImage.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+const BackgroundImage = ({ imageUrl, height }) => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: height,
+        width: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        zIndex: -1, 
+      }}
+    />
+  );
+};
+
+export default BackgroundImage;
+
+BackgroundImage.propTypes = {
+  imageUrl: PropTypes.string.isRequired, 
+  height: PropTypes.string.isRequired,   
+};
