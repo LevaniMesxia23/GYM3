58e38e27a4336c39d222e5f7e7dcfea79dd71387
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-10-25 02:00:28 +0400

    style: add desktop navigation and CustomersInfo

--- src/components/Header/CustomersInfo.jsx
+++ src/components/Header/CustomersInfo.jsx
@@ -1,6 +1,14 @@
+import Star from "../../../public/star.svg"
+import { useContext } from "react";
+import { Mycontext } from "../../context/context";
+
 function CustomersInfo() {
+  const {isTablet} = useContext(Mycontext)
   return (
     <div className="bg-[#D7FD44] h-[6.25rem] mt-[348px] flex px-10 justify-evenly gap-3">
+      {isTablet && <div className="flex ">
+        <img src={Star} className="w-[2.77388rem]" alt="" />
+      </div>}
       <div className="flex items-center justify-center text-center ">
         <p>
           <b>200 + </b>Happy Customers
@@ -12,6 +20,9 @@ function CustomersInfo() {
       <div className="flex items-center justify-center text-center ">
         <p><b>98 %</b> Customer Satisfaction</p>
       </div>
+      {isTablet && <div className="flex ">
+        <img src={Star} className="w-[2.77388rem]" alt="" />
+      </div>}
     </div>
   );
 }
