50c4786b809028a5511e939f015a8de6d6e7e831
LK777-D <luka.dolbaia17@gmail.com>
2024-10-25 18:31:08 +0400

    slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Header/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Services from "./pages/Services";
-import Stores from "./pages/Stores";
+import Stories from "./pages/Stories";
 import { MyProvider } from "./context/context";
+import Layout from "./components/UI/Layout";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <MyProvider>
-          <Routes>
-            <Route path="/" element={<Home />} />
+      <MyProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Home />} />
             <Route path="/services" element={<Services />} />
-            <Route path="/stores" element={<Stores />} />
+            <Route path="/stories" element={<Stories />} />
             <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </MyProvider>
-      </BrowserRouter>
+          </Route>
+        </Routes>
+      </MyProvider>
     </>
   );
 }
