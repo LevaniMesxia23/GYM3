39959d0bd44b97a713857222162ddac9c9f5c32d
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-11-01 19:53:25 +0400

    feat: add Skeleton on Prices

--- src/components/Prices/Prices.jsx
+++ src/components/Prices/Prices.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import DownArrow from "../../../public/downArrow.svg";
 import { usePrice } from "../../hooks/usePrice";
 import ClockLoader from "react-spinners/ClockLoader";
+import Skeleton from "react-loading-skeleton";
 
 function Prices() {
   const { data, error, isLoading } = usePrice();
@@ -9,13 +10,7 @@ function Prices() {
   const [showAll, setShowAll] = useState(false);
 
   if (error) return <p className="text-red-500">{error.message}</p>;
-  if (isLoading)
-    return (
-      <div className="flex justify-center items-center h-[6.25rem]">
-        <ClockLoader color="#D7FD44" size={80} />
-      </div>
-    );
-
+  
   const priceData = data?.about || [];
   const visibleData = showAll ? priceData : priceData.slice(0, 6);
 
@@ -40,50 +35,70 @@ function Prices() {
       >
         Prices
       </p>
-      {visibleData.map((item, index) => (
-        <div key={index} className="w-full mt-7">
-          <div
-            className="bg-[#222] rounded-[8.75rem] px-8 py-6 flex items-center justify-between cursor-pointer"
-            onClick={() => handleToggle(index)}
-          >
-            <p className="text-[1.25rem] font-bold text-[#C4C4C4] uppercase">
-              {item.name}
-            </p>
-            <div className="bg-[#D7FD44] rounded-full w-[5rem] h-[5rem] flex items-center justify-center">
-              <img
-                src={DownArrow}
-                alt="Expand section"
-                className={`transition-transform duration-300 ${
-                  arrowClick.includes(index) ? "rotate-[49deg]" : "rotate-0"
-                }`}
-              />
+      
+      {isLoading ? (
+        Array.from({ length: showAll ? priceData.length : 6 }).map((_, index) => (
+          <div key={index} className="w-full mt-7">
+            <div className="bg-[#222] rounded-[8.75rem] px-8 py-6 flex items-center justify-between">
+              <Skeleton className="w-1/2 h-[2rem]" />
+              <Skeleton circle={true} className="w-[5rem] h-[5rem]" />
+            </div>
+            <div className={`overflow-hidden transition-all duration-500 ease-in-out max-h-0 opacity-0`}>
+              <div className="flex flex-col md:flex-row lg:flex-row md:justify-around lg:justify-around md:py-8 lg:py-8 bg-[#333] p-4 pl-8 mt-4 text-white border border-[#D7FD44] rounded-[8.75rem] transition-all duration-500 ease-in-out">
+                <Skeleton className="w-full h-[2rem]" />
+                <Skeleton className="w-full h-[2rem]" />
+                <Skeleton className="w-full h-[2rem]" />
+              </div>
             </div>
           </div>
-          <div
-            className={`overflow-hidden transition-all duration-500 ease-in-out ${
-              arrowClick.includes(index)
-                ? "max-h-[500px] opacity-100"
-                : "max-h-0 opacity-0"
-            }`}
-            style={{ transformOrigin: "top" }}
-          >
-            <div className="flex flex-col md:flex-row lg:flex-row md:justify-around lg:justify-around md:py-8 lg:py-8 bg-[#333] p-4 pl-8 mt-4 text-white border border-[#D7FD44] rounded-[8.75rem] transition-all duration-500 ease-in-out">
-              <p className="font-bold md:text-[1.25rem] lg:text-[1.5rem]">
-                <b className="text-[#D7FD44] font-extrabold">X</b> 1 session - $
-                {item.sessions_single}
+        ))
+      ) : (
+        visibleData.map((item, index) => (
+          <div key={index} className="w-full mt-7">
+            <div
+              className="bg-[#222] rounded-[8.75rem] px-8 py-6 flex items-center justify-between cursor-pointer"
+              onClick={() => handleToggle(index)}
+            >
+              <p className="text-[1.25rem] font-bold text-[#C4C4C4] uppercase">
+                {item.name}
               </p>
-              <p className="font-bold md:text-[1.25rem] lg:text-[1.5rem]">
-                <b className="text-[#D7FD44] font-extrabold">X</b> 5 sessions -
-                ${item.sessions_five}
-              </p>
-              <p className="font-bold md:text-[1.25rem] lg:text-[1.5rem]">
-                <b className="text-[#D7FD44] font-extrabold">X</b> 10 sessions -
-                ${item.sessions_ten}
-              </p>
+              <div className="bg-[#D7FD44] rounded-full w-[5rem] h-[5rem] flex items-center justify-center">
+                <img
+                  src={DownArrow}
+                  alt="Expand section"
+                  className={`transition-transform duration-300 ${
+                    arrowClick.includes(index) ? "rotate-[49deg]" : "rotate-0"
+                  }`}
+                />
+              </div>
+            </div>
+            <div
+              className={`overflow-hidden transition-all duration-500 ease-in-out ${
+                arrowClick.includes(index)
+                  ? "max-h-[500px] opacity-100"
+                  : "max-h-0 opacity-0"
+              }`}
+              style={{ transformOrigin: "top" }}
+            >
+              <div className="flex flex-col md:flex-row lg:flex-row md:justify-around lg:justify-around md:py-8 lg:py-8 bg-[#333] p-4 pl-8 mt-4 text-white border border-[#D7FD44] rounded-[8.75rem] transition-all duration-500 ease-in-out">
+                <p className="font-bold md:text-[1.25rem] lg:text-[1.5rem]">
+                  <b className="text-[#D7FD44] font-extrabold">X</b> 1 session - $
+                  {item.sessions_single}
+                </p>
+                <p className="font-bold md:text-[1.25rem] lg:text-[1.5rem]">
+                  <b className="text-[#D7FD44] font-extrabold">X</b> 5 sessions -
+                  ${item.sessions_five}
+                </p>
+                <p className="font-bold md:text-[1.25rem] lg:text-[1.5rem]">
+                  <b className="text-[#D7FD44] font-extrabold">X</b> 10 sessions -
+                  ${item.sessions_ten}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
+
       {priceData.length > 6 && !showAll ? (
         <button
           onClick={() => setShowAll(true)}
