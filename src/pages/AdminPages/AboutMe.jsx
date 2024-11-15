f24cddd13b67f83827ceac09bd68f7baaae460a2
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-11-15 16:14:09 +0400

    style: add AboutFormsData component

--- src/pages/AdminPages/AboutMe.jsx
+++ src/pages/AdminPages/AboutMe.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutHeader from '../../components/AdminPanel/AdminAbout/AboutHeader'
+import AboutForms from '../../components/AdminPanel/AdminAbout/AboutForms'
 
 export default function AboutMe() {
   return (
     <div className="px-[5rem]">
       <AboutHeader />
+      <AboutForms />
     </div>
   )
 }
