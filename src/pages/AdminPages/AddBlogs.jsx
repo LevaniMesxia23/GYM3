420dd414aabb47f27f4d21878c2644698781b69f
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-11-29 18:29:13 +0400

    style: add adminPanel AddBlogs UI

--- src/pages/AdminPages/AddBlogs.jsx
+++ src/pages/AdminPages/AddBlogs.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import AdminBlog from '../../components/AdminPanel/AdminBlog/AdminBlog'
 
 function AddBlogs() {
   return (
     <div>
-      
+      <AdminBlog />
     </div>
   )
 }
